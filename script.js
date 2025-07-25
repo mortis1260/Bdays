05fba33114f84385905dc8a70aca8b6fb244be6a
mortis1260 <mortisthemortal1260@gmail.com>
2025-07-25 21:47:30 +0530

    Done ig?

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById('revealButton').addEventListener('click', function() {
     var message = document.getElementById('specialMessage');
     if (message.style.display === 'none') {
         message.style.display = 'block';
-        this.textContent = "💗  ";
+        this.textContent = "💖";
     } else {
         message.style.display = 'none';
         this.textContent = "Click Here";
